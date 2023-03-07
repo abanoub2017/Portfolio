38a594a61ff3e515083278b10bfcdd3f12595a7c
AbanoubGeorgeFawzy <age@crossworkers.com>
2023-03-07 17:23:52 +0200

    add Toast Compoenent && create .env file

--- src/components/utils/AbToast.vue
+++ src/components/utils/AbToast.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="toast-bottom-left" v-if="loadingToast"
+    <div id="toast-bottom-left" 
         class="fixed  bottom-5 right-5 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
         role="alert">
         <div
@@ -12,8 +12,8 @@
             </svg>
             <span class="sr-only">Fire icon</span>
         </div>
-        <div class="ml-3 text-sm font-normal">Message sent successfully.</div>
-        <button type="button" @click="closeToast()"
+        <div class="ml-3 text-sm font-normal">{{ message }}</div>
+        <button type="button" @click="$emit('closeToast')"
             class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
             data-dismiss-target="#toast-default" aria-label="Close">
             <span class="sr-only">Close</span>
@@ -28,6 +28,14 @@
 </template>
 
 <script setup>
+const props = defineProps({
+    message:{
+        type: String,
+        default:'Message sent successfully'
+    }
+})
+ 
+const emit = defineEmits(['closeToast'])
 
 </script>
 
